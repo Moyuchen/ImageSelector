87e78e243843ca713fb3ebac0cbe3314c06aaf9b
zhangyabo <zhangyabo@yimeijian.cn>
2020-03-18 15:13:56 +0800

    全部上传

---
+++ app/src/main/java/com/moyuchen/myapplication/LauncherActivity.java
@@ -0,0 +1,15 @@
+package com.moyuchen.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import me.nereo.multi_image_selector.MultiImageSelector;
+
+import android.os.Bundle;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+}
